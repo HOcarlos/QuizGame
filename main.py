e210d47654f70c340b13605581f85b7b32ae8997
HOcarlos <carlos.ho96@outlook.com>
2021-02-11 10:48:56 -0300

    Final Version

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ for question in question_data:
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
+
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
